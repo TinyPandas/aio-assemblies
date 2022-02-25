da1095c0f5320fc969c7e6de81a137e6b23caef0
Chris Lallo <chrislallo03@gmail.com>
2022-02-25 02:23:21 -0500

    Added color change
    from 0.25 to 0.27

--- prototypes/items/aio1.lua
+++ prototypes/items/aio1.lua
@@ -5,7 +5,7 @@ aioAssembler1.place_result = "aio-assembly-1"
 aioAssembler1.icons = {
     {
         icon = aioAssembler1.icon,
-        tint = {r=0.25,g=0.5,b=0.25,a=0.3}
+        tint = {r=0.27,g=0.5,b=0.25,a=0.3}
     },
 }
 
